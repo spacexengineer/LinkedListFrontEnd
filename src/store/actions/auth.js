0a48dc1fea7466ceeae84b831fba714b0e1ffb43
Elie Schoppik <eschoppik@gmail.com>
2018-04-24 14:19:39 -0700

    init

---
+++ src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import { apiCall, setTokenHeader } from "../../services/api";
+import { addError, removeError } from "./errors";
+import { SET_CURRENT_USER } from "../actionTypes";
+
+export function setAuthorizationToken(token) {
+  setTokenHeader(token);
+}
+/**
+ * Sign up or Sign in a user
+ * @param {string} type signin or signup user
+ * @param {object} userData JSON object from form
+ */
+export function authUser(type, userData) {
+  return dispatch => {
+    // wrap our thunk in a promise so we can wait for the API call
+    return new Promise((resolve, reject) => {
+      return apiCall("post", `/api/auth/${type}`, userData)
+        .then(({ token, ...user }) => {
+          localStorage.setItem("jwtToken", token);
+          setAuthorizationToken(token);
+          dispatch(setCurrentUser(user));
+          dispatch(removeError());
+          resolve(); // indicate that the API call succeeded
+        })
+        .catch(err => {
+          dispatch(addError(err.message));
+          reject(); // indicate the API call failed
+        });
+    });
+  };
+}
+
+export function logout() {
+  return dispatch => {
+    localStorage.clear();
+    setAuthorizationToken(false);
+    dispatch(setCurrentUser({}));
+  };
+}
+
+export function setCurrentUser(user) {
+  return {
+    type: SET_CURRENT_USER,
+    user
+  };
+}
